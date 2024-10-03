1c50a8e662a16c2ef63081260043c90e01377dac
rachmadrf <rachmad17845@gmail.com>
2024-10-03 08:07:01 +0700

    first commit

---
+++ Pemilihan2Percobaan1_1_21.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_1_21 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner (System.in);
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = input21.nextInt();
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 ==0) {
+                if (tahun % 400 ==0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
